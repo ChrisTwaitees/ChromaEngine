7e271c27b8de6ac2376228023ecdc9fe18f00e7b
Chris Thwaites <chrthw@gmail.com>
2019-11-07 12:21:24 +0000

    temp commit

--- Chroma/thirdparty/include/glm/out/build/x64-Debug/cmake_install.cmake
+++ Chroma/thirdparty/include/glm/out/build/x64-Debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "c:/users/cthwaites/source/repos/chromaengine/chroma/thirdparty/include/glm/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/cthwaites/Source/Repos/ChromaEngine/Chroma/thirdparty/include/glm/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
