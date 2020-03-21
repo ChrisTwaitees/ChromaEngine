dd996219eab66d702746e3025c0e425d946b4985
Chris Thwaites <chrthw@gmail.com>
2020-03-21 16:06:42 +0000

    skinned vert now inherits from base vert

--- Chroma/Chroma/source/Render/model/SkinnedVertex.h
+++ Chroma/Chroma/source/Render/model/SkinnedVertex.h
@@ -3,14 +3,10 @@
 
 //common
 #include <common/PrecompiledHeader.h>
+#include <model/Vertex.h>
 
-struct ChromaSkinnedVertex
+struct ChromaSkinnedVertex : public ChromaVertex
 {
-	glm::vec3 m_position{ 0.0 };
-	glm::vec3 m_normal{ 0.0 };
-	glm::vec3 m_tangent{ 0.0 };
-	glm::vec3 m_bitangent{ 0.0 };
-	glm::vec2 m_texCoords{ 0.0 };
 	glm::ivec4 m_jointIDs { 0 };
 	glm::vec4 m_jointWeights{ 0.0 };
 };
