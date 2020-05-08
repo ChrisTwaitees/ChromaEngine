8f1e6afa1d0a88806ff67b5758dfd69eb8c8c425
Chris Thwaites <chrthw@gmail.com>
2020-05-08 16:23:00 +0100

    Starting uniform buffer objects

---
+++ Chroma/Chroma/source/Render/shader/UniformBuffer.h
@@ -0,0 +1,15 @@
+#ifndef CHROMA_UNIFORM_BUFFER_H
+#define CHROMA_UNIFORM_BUFFER_H
+#include <common/PrecompiledHeader.h>
+
+class UniformBuffer
+{
+public:
+	UniformBuffer() {};
+	~UniformBuffer() {};
+
+private:
+
+};
+
+#endif // CHROMA_UNIFORM_BUFFER_H
